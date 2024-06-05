6d10121e8cad9e0cbcda752ec1a64f3fc492a277
StarryMic <alexander.r.elledge@gmail.com>
2024-06-05 18:02:08 -0400

    First push.

---
+++ Scripts/AutoURLButton.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+using System.Diagnostics;
+
+public partial class AutoURLButton : Button
+{
+    [Export] private string _url;
+    public override void _Ready()
+    {
+        this.Pressed += OnPressed; // When button is pressed, open URL.
+    }
+
+    private void OnPressed()
+    {
+        Debug.Assert(_url != null, nameof(_url) + " != null");
+        OS.ShellOpen(_url);
+    }
+}
